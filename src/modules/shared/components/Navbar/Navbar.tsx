290c83ce1f7570b5e1d3b8e4b0ce976eaebb0b1b
ziadyasserr <zyasser283@gmail.com>
2024-11-30 18:37:34 +0200

    Navbar

--- src/modules/shared/components/Navbar/Navbar.tsx
+++ src/modules/shared/components/Navbar/Navbar.tsx
@@ -1,7 +1,68 @@
-import React from 'react'
+import { useContext, useState } from 'react';
+import { HiMiniBellAlert } from 'react-icons/hi2';
+import { RiArrowDropDownLine } from 'react-icons/ri';
+import { useNavigate } from 'react-router-dom';
+import human from '../../../../assets/human.png';
+import logo from '../../../../assets/logo.png';
+import { AuthContext } from '../../../../context/AuthContext/AuthContext';
 
 export default function Navbar() {
+  const { loginData } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const handleLogout = () => {
+    localStorage.removeItem('token'); // Clear token
+    navigate('/login'); // Redirect to login page
+  };
+
   return (
-    <div>Navbar</div>
-  )
+    <>
+      <div className="flex justify-between items-center bg-gray-50 shadow-lg py-1 mb-10 px-5">
+        <div>
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="flex items-center gap-5">
+          <div>
+            <HiMiniBellAlert className="text-yellow-500 text-3xl " />
+          </div>
+          <div className="border-r border-[#9A9A9A] h-8"></div>
+
+          <div className="flex items-center gap-5">
+            <div>
+              <img
+                src={human}
+                alt="my-pic"
+                className="w-10 h-10 rounded-full"
+              />
+            </div>
+            <div>
+              <span className="block font-semibold">{loginData?.userName}</span>
+              <span className="block text-gray-500 text-sm">
+                {loginData?.userEmail}
+              </span>
+            </div>
+            <div className="relative">
+              <button
+                onClick={() => setIsDropdownOpen((prev) => !prev)}
+                // className="text-gray-700 hover:text-gray-900 focus:outline-non"
+              >
+                <RiArrowDropDownLine className="text-2xl" />
+              </button>
+              {isDropdownOpen && (
+                <div className="absolute right-0 mt-3 w-40 bg-white border border-gray-200 rounded shadow-md z-10">
+                  <button
+                    onClick={handleLogout}
+                    className="w-full text-left px-4 py-2 text-sm  hover:bg-red-50 hover:text-"
+                  >
+                    Logout
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/modules/shared/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/modules/shared/components/ProtectedRoute/ProtectedRoute.tsx
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import { AuthContext } from '../../../../context/AuthContext/AuthContext';
 
-export default function ProtectedRoute() {
+export default function ProtectedRoute({children}) {
   let { loginData } = useContext(AuthContext);
   if (localStorage.getItem('token') || loginData) return children;
   else return <Navigate to="/login" />;
